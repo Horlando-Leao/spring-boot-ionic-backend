655de749915f8cd15a9a95ea9ad8ff3c2d8af05f
Horlando Leão <horlandoleao8@gmail.com>
2021-03-06 00:00:17 -0300

    mapeamento de herança em pagamento

--- src/main/java/com/horlando/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/horlando/cursomc/domain/PagamentoComCartao.java
@@ -1,7 +1,10 @@
 package com.horlando.cursomc.domain;
 
+import javax.persistence.Entity;
+
 import com.horlando.cursomc.domain.enums.EstadoPagamento;
 
+@Entity
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
