5a9416388378a51a671770d0a15e9039cf0f77b0
Horlando Leão <horlandoleao8@gmail.com>
2021-03-11 00:02:56 -0300

    Endpoint de paginacao com parametros opcionais na requisicao

--- src/main/java/com/horlando/cursomc/CursomcApplication.java
+++ src/main/java/com/horlando/cursomc/CursomcApplication.java
@@ -63,6 +63,11 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Categoria cat1 = new Categoria(null, "informatica");//estancias os objetos
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat4 = new Categoria(null, "Eletrônicos");
+		Categoria cat5 = new Categoria(null, "Jardinagem");
+		Categoria cat6 = new Categoria(null, "Decoração");
+		Categoria cat7 = new Categoria(null, "Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 5000.00);//estancias os objetos
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -75,7 +80,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));//salvando no banco
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));//salvando no banco
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");//estancias os objetos
