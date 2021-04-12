27200d17bd05d78b90c5c4f902b7967d520e1c07
Horlando Leão <horlandoleao8@gmail.com>
2021-04-12 00:22:55 -0300

    validacao sintatica com Bean validation

--- src/main/java/com/horlando/cursomc/services/CategoriaService.java
+++ src/main/java/com/horlando/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.horlando.cursomc.domain.Categoria;
+import com.horlando.cursomc.dto.CategoriaDTO;
 import com.horlando.cursomc.repositories.CategoriaRepository;
 import com.horlando.cursomc.services.exceptions.DataIntegrityException;
 import com.horlando.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,11 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	//metodo auxiliar que instancia uma categoria a partir de um DTO
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
